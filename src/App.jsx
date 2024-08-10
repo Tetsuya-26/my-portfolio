e33c904261a8390065357efae6be73f935d93e7c
unknown <IT-1@IT-1.grj.intranet>
2024-08-10 10:16:16 +0800

    Updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,20 @@
 import React from 'react'
 import Navbar from './Navbar'
+import { motion } from 'framer-motion'
 
 function App() {
 
   return (
-    <>
-      <div>
+      <motion.div
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      transition={{
+        duration: 0.9,
+        delay: 0.6,
+        ease: [0, 0.71, 0.2, 1.01]
+      }}>
         <Navbar/>
-      </div>
-    </>
+      </motion.div>
   )
 }
 
--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -13,6 +13,7 @@ const Navbar = () => {
         </div>
       </div>
     </nav>
+    
   );
 };
 
